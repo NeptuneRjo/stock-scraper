3cf0498bd540988d970d3a3dda7e73aff4177cf1
NeptuneRjo <rjo9000@outlook.com>
2022-09-22 19:32:22 -0400

    add api controllers

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
+import {
+	get_top_fifty_coins,
+	get_all_coins,
+} from '../controllers/apiControllers'
 
 const router = Router()
 
-router.route('/coins').get((req, res) => res.send('Get coins'))
+router.route('/all-coins').get(get_all_coins)
+router.route('/top-50').get(get_top_fifty_coins)
 router.route('/coin/:id').get((req, res) => res.send('get url'))
 
 export default router
