cbc9f084041316eb3ea09f51ee58ba4918fe36ae
NeptuneRjo <rjo9000@outlook.com>
2022-09-23 00:23:44 -0400

    add controller to single coin route

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express'
 import {
 	get_top_fifty_coins,
 	get_all_coins,
+	get_single_coin,
 } from '../controllers/apiControllers'
 
 const router = Router()
 
 router.route('/all-coins').get(get_all_coins)
 router.route('/top-50').get(get_top_fifty_coins)
-router.route('/coin/:id').get((req, res) => res.send('get url'))
+router.route('/coin/:id').get(get_single_coin)
 
 export default router
