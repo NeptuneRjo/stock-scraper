a6b817d546c05590fe8f0c883e9e30b5096bc3f3
NeptuneRjo <rjo9000@outlook.com>
2022-09-24 22:41:59 -0400

    add tests

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -1,33 +1,9 @@
 import 'jest'
 import request from 'supertest'
 import express from 'express'
-import { scrapeAllCoins } from '../scraper/scrapeAllCoins'
-import { scrapeSingleCoin } from '../scraper/scrapeSingleCoin'
 
 const app = express()
 const server = request(app)
-
-type Coin = {
-	image: string
-	name: {
-		name: string
-		symbol: string
-	}
-	price: string
-	priceHistory: {
-		hour: string
-		day: string
-		week: string
-	}
-	marketCap: string
-	dayVolume: {
-		USD: string
-		coin: string
-	}
-	rank: string
-	circulation: string
-	id: string
-}
 
 describe('API tests', () => {
 	it('GETs /all-coins', async () => {
